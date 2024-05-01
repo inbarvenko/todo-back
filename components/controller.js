36ac4a214181a1c233892580f45f38910d388f5b
inbarvenko <Inga_barvenko@mail.ru>
2024-05-01 22:28:23 +0300

    added edit requests

--- components/controller.js
+++ components/controller.js
@@ -3,7 +3,7 @@ const db = require('../db/connection.js');
 const todosController = {
   getAllTodoTasks: async (todosid) => {
     return await db
-      .many("SELECT * FROM tasks WHERE todosid = ($1)", todosid)
+      .any("SELECT * FROM tasks WHERE todosid = ($1)", todosid)
       .then((data) => {
         // console.log("DATA:", data);
         return data;
@@ -38,22 +38,86 @@ const todosController = {
     console.log(title, todo_id);
     return await db
       .one("INSERT INTO tasks (todosid, title) VALUES ($1, $2) RETURNING *", [todo_id, title])
-      .then(() => {
+      .then((data) => {
         console.log("ADDED TASK");
+        return data;
       })
       .catch((error) => {
         console.log("ERROR:", error);
       });
   },
   createTodo: async (title) => {
-    console.log(title);
     return await db
-      .one(`INSERT INTO todos (title) VALUES ($1)`, title)
-      .then(() => {
+      .one(`INSERT INTO todos (title) VALUES ($1) RETURNING *`, title)
+      .then((data) => {
         console.log("ADDED TODO");
+        return data;
       })
       .catch((error) => {
         console.log("ERROR:", error);
+        return false;
+      });
+  },
+  removeTodo: async (todo_id) => {
+    return await db
+      .none(`DELETE FROM todos WHERE id = ($1)`, todo_id)
+      .then(() => {
+        console.log("DELETED TODO");
+        return true;
+      })
+      .catch((error) => {
+        console.log("ERROR:", error);
+        return false;
+      });
+  },
+  removeTask: async (task_id) => {
+    return await db
+      .none(`DELETE FROM tasks WHERE id = ($1)`, task_id)
+      .then(() => {
+        console.log("DELETED TASK");
+        return true;
+      })
+      .catch((error) => {
+        console.log("ERROR:", error);
+        return false;
+      });
+  },
+  toggleStatus: async (status, task_id) => {
+    return await db
+      .none(`UPDATE tasks SET active = ($1) WHERE id = ($2)`, [status, task_id])
+      .then(() => {
+        console.log("TOGGLED STATUS");
+        return true;
+      })
+      .catch((error) => {
+        console.log("ERROR:", error);
+        return false;
+      });
+  },
+  changeTitleTodo: async (todosid, title) => {
+
+    console.log(title);
+    return await db
+      .none(`UPDATE todos SET title = ($1) WHERE id = ($2)`, [title, todosid])
+      .then(() => {
+        console.log("CHANGED TITLE TODO");
+        return true;
+      })
+      .catch((error) => {
+        console.log("ERROR:", error);
+        return false;
+      });
+  },
+  changeTitleTask: async (taskid, title) => {
+    return await db
+      .none(`UPDATE tasks SET title = ($1) WHERE id = ($2)`, [title, taskid])
+      .then(() => {
+        console.log("CHANGED TITLE TASK");
+        return true;
+      })
+      .catch((error) => {
+        console.log("ERROR:", error);
+        return false;
       });
   }
 };
--- components/routers.js
+++ components/routers.js
@@ -1,52 +1,102 @@
-
 const express = require("express");
 const router = express.Router();
-const todosController = require('./controller');
+const todosController = require("./controller");
 
 router.get("/:todoId", async (req, res) => {
-	try {
-		const todoId = req.params.todoId;
-		const todoInfo = await todosController.getTodo(todoId);
-		const todoTasks = await todosController.getAllTodoTasks(todoId);
-		res.json({todoInfo, todoTasks});
-	}
-	catch (error) {
-		res.status(400).send(error.message);
-	}
+  try {
+    const todoId = req.params.todoId;
+    // const todoInfo = await todosController.getTodo(todoId);
+    const todoTasks = await todosController.getAllTodoTasks(todoId);
+    if (todoTasks) {
+      res.json({ todoTasks });
+    }
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
 });
 
 router.get("/", async (req, res) => {
-	try {
-		const todos = await todosController.getAllTodos();
-		res.json({todos});
-	}
-	catch (error) {
-		res.status(400).send(error.message);
-	}
+  try {
+    const todos = await todosController.getAllTodos();
+    if (todos) {
+      res.json({ todos });
+    }
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
 });
 
 router.post("/:todosId", async (req, res) => {
-	try {
-		const title = req.query.title;
-		const todosId = req.params.todosId;
-		const obj = await todosController.createTask(title, todosId);
-		res.json(obj);
-	}
-	catch (error) {
-		res.status(400).send(error.message);
-	}
+  try {
+    const title = req.query.title;
+    const todosId = req.params.todosId;
+    const obj = await todosController.createTask(title, todosId);
+    res.json(obj);
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
 });
 
 router.post("/", async (req, res) => {
+  try {
+    const title = req.query.title;
+    const obj = await todosController.createTodo(title);
+    res.json(obj);
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+});
+
+router.delete("/:todosId", async (req, res) => {
+  try {
+    const todosId = req.params.todosId;
+    const obj = await todosController.removeTodo(todosId);
+    res.json(obj);
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+});
+
+router.delete("/tasks/:taskId", async (req, res) => {
+  try {
+    const taskId = req.params.taskId;
+    const obj = await todosController.removeTask(taskId);
+    res.json(obj);
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+});
+
+router.patch("/:todosId", async (req, res) => {
 	try {
 		const title = req.query.title;
-		console.log('req >>>', req.query);
-		const obj = await todosController.createTodo(title);
+		const todosId = req.params.todosId;
+		const obj = await todosController.changeTitleTodo(todosId, title);
 		res.json(obj);
-	}
-	catch (error) {
+	} catch (error) {
 		res.status(400).send(error.message);
 	}
+	
+})
+
+router.patch("/tasks/:taskId", async (req, res) => {
+  try {
+    const taskId = req.params.taskId;
+    const status = req.query.status;
+	const title = req.query.title;
+	let obj;
+
+	console.log(status, title);
+
+	if (title) {
+		obj = await todosController.changeTitleTask(taskId, title);
+	} else {
+		obj = await todosController.toggleStatus(status, taskId);
+	}
+    res.json(obj);
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
 });
 
 module.exports = router;
